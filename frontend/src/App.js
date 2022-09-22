aadfdc3819a6872c16ed8feb910ced236e40da05
Daniel - Laptop <Daniel.dy.xu@gmail.com>
2022-09-22 16:49:29 -0400

    Created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ShopOnline</a>
+      </header>
+      <main>List Products Here!!!</main>
+    </div>
+  );
+}
+
+export default App;
