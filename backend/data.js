31a41882ddd599e87e4a089d4a1fc288eeb8213f
Daniel - Laptop <Daniel.dy.xu@gmail.com>
2022-09-27 18:25:30 -0400

    improved add to cart function

--- backend/data.js
+++ backend/data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Peanut X Kaws Shirt',
       slug: 'peanut-x-kaws-shirt',
       category: 'Shirts',
@@ -13,6 +14,7 @@ const data = {
       description: 'High quality shirt from Uniqlo X Kaws collab',
     },
     {
+      _id: '2',
       name: 'Peanut X Kaws Plushie',
       slug: 'peanut-x-kaws-plush',
       category: 'Plushies',
@@ -25,6 +27,7 @@ const data = {
       description: 'High quality Uniqlo X Kaws plushie',
     },
     {
+      _id: '3',
       name: 'Peanut X Kaws Sweater',
       slug: 'peanut-x-kaws-sweater',
       category: 'Sweater',
@@ -37,6 +40,7 @@ const data = {
       description: 'High quality sweater from Uniqlo X Kaws collab',
     },
     {
+      _id: '4',
       name: 'Peanut X Kaws Tote Bag',
       slug: 'peanut-x-kaws-tote-bag',
       category: 'Bags',
